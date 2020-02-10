ae68fcbc18341e09c6d68ebfd279b3be0d0e74ee
johannes <johannes.nyman@csc.fi>
2020-02-10 11:41:20 +0200

    Initial commit

---
+++ src/main/java/avaa/Dataset.java
@@ -0,0 +1,31 @@
+package avaa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+@Entity
+public class Dataset extends PanacheEntityBase{
+    @Id
+    public String data_id;
+    public String org_abbreviation;
+    public String org_fin;
+    public String org_eng;
+    public String name_fin;
+    public String name_eng;
+    public String scale;
+    public String year;
+    public String format_fin;
+    public String coord_sys;
+    public Integer map_sheets;
+    public Integer file_size;
+    public String data_url;
+    public Boolean queries;
+    public Integer data_max_scale;
+    public String meta;
+    public Integer access;
+    public String license_url;
+    public String format_eng;
+    public String funet;
+}
