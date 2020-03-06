3acb860e5bc55c7df8884b656708228f3ba9aded
Mikko Kautto <mkautto@users.noreply.github.com>
2020-03-06 09:20:13 +0200

    AVAA-1029: Sending email (#5)
    * Move email template to resource bundle
    
    * Format email body
    
    * Mailer config
    
    * Bump Quarkus version
    
    * Sending email
    
    * Test sending email

--- src/main/java/fi/csc/avaa/paituli/constants/Constants.java
+++ src/main/java/fi/csc/avaa/paituli/constants/Constants.java
@@ -2,6 +2,9 @@ package fi.csc.avaa.paituli.constants;
 
 public final class Constants {
 
+    public static final String MESSAGE_KEY_EMAIL_SUBJECT = "email_subject";
+    public static final String MESSAGE_KEY_EMAIL_BODY_TEMPLATE = "email_body_template";
+
     public static final String PATH_DATASETS = "/datasets";
     public static final String PATH_DOWNLOAD = "/download";
 }
---
+++ src/main/java/fi/csc/avaa/paituli/service/EmailService.java
@@ -0,0 +1,43 @@
+package fi.csc.avaa.paituli.service;
+
+import fi.csc.avaa.paituli.constants.Constants;
+import fi.csc.avaa.paituli.model.DownloadRequest;
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.ReactiveMailer;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.core.Response;
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.StringJoiner;
+import java.util.concurrent.CompletionStage;
+
+@ApplicationScoped
+public class EmailService {
+
+    @Inject
+    ReactiveMailer mailer;
+
+    public CompletionStage<Response> sendEmail(String language, DownloadRequest request,
+                                               List<String> filenameList, String zipUrl) {
+        ResourceBundle messages = ResourceBundle.getBundle("messages", Locale.forLanguageTag(language));
+        String subject = messages.getString(Constants.MESSAGE_KEY_EMAIL_SUBJECT);
+        String template = messages.getString(Constants.MESSAGE_KEY_EMAIL_BODY_TEMPLATE);
+
+        StringJoiner datasetInfo = new StringJoiner(", ");
+        if (request.org != null) datasetInfo.add(request.org);
+        if (request.data != null) datasetInfo.add(request.data);
+        if (request.scale != null) datasetInfo.add(request.scale);
+        if (request.year != null) datasetInfo.add(request.year);
+        if (request.coordsys != null) datasetInfo.add(request.coordsys);
+        if (request.format != null) datasetInfo.add(request.format);
+
+        String filenames = "<br>" + String.join("<br>", filenameList) + ".";
+        String body = MessageFormat.format(template, datasetInfo.toString(), filenames, zipUrl);
+
+        return mailer.send(Mail.withHtml(request.email, subject, body)).thenApply(x -> Response.accepted().build());
+    }
+}
