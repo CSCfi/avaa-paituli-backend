fff2b632315a3b986995829148269249ba5afff8
johannesnyman <54168685+johannesnyman@users.noreply.github.com>
2020-12-11 10:16:47 +0200

    Update DownloadGenerator.java

--- src/main/java/fi/csc/avaa/paituli/download/DownloadGenerator.java
+++ src/main/java/fi/csc/avaa/paituli/download/DownloadGenerator.java
@@ -77,7 +77,7 @@ public class DownloadGenerator {
     private List<String> collectAbsolutePaths(List<String> filePaths) {
         List<String> absolutePaths = new ArrayList<>();
         filePaths.forEach(filePath -> {
-            String absolutePath = String.format("%s/%s", inputPath, filePath);
+            String absolutePath = String.format("%s%s", inputPath, filePath);
             if (absolutePath.contains("*")) {
                 absolutePaths.addAll(findMatchingFiles(absolutePath));
             } else {
