486ea158291cdedb161c05f5174c883688dc368d
Kylli Ek <kylli.ek@csc.fi>
2021-09-24 11:29:17 +0300

    Syntax change to shorter + error catch

--- src/main/java/fi/csc/avaa/paituli/service/DatasetService.java
+++ src/main/java/fi/csc/avaa/paituli/service/DatasetService.java
@@ -15,11 +15,21 @@ public class DatasetService {
 
     @Transactional
     public List<LocalizedDataset> getLocalizedDatasets(Locale locale) {
-        try (Stream<Dataset> stream = Dataset.streamAll()) {
+        try (Stream<Dataset> stream = Dataset.stream("access", 1)) {
             return stream
-                    .filter(dataset -> dataset.getAccess() == 1)
                     .map(dataset -> LocalizedDataset.from(dataset, locale))
                     .collect(Collectors.toList());
         }
+        catch (Exception e)
+24         {
+25             System.out.println("Test4");
+26             // printStackTrace method
+27             // prints line numbers + call stack
+28             e.printStackTrace();
+29
+30             // Prints what exception has been thrown
+31             System.out.println(e);
+32             return null;
+33         }
     }
 }
